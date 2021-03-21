133d665eb87b1f8e9421f8de3e361b02fb14c413
Remus Courtenay <remuscourtenay@gmail.com>
2021-03-22 10:31:06 +1300

    Added new layout for input data activity

--- app/src/main/java/com/example/waimeaHarriersDataInput/InputDataActivity.java
+++ app/src/main/java/com/example/waimeaHarriersDataInput/InputDataActivity.java
@@ -5,7 +5,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.api.services.sheets.v4.model.ValueRange;
@@ -15,13 +18,17 @@ import java.util.List;
 public class InputDataActivity extends Activity {
 
     // Bundle key for input number
-    private static final String INPUT_NUM_BUNDLE_KEY = "input_num";
+    private static final String BIB_NUM_BUNDLE_KEY = "bib_num";
 
-    private int inputNum;
+    private int bibNumber;
 
-    private EditText mInputNumberEditText;
+    private Button mDecrementFinishNumButton;
+
+    private EditText mInputBibNumberEditText;
     private TextView mOutputNameTextView;
-    private TextView mOutputCompetitorTypeTextView;
+    private Spinner mOutputCompetitorTypeSpinner;
+    private CheckBox mOutputOwnTimeCheckBox;
+    private EditText mOutputOwnTimeEditText;
 
     private SheetReader reader;
 
@@ -30,24 +37,28 @@ public class InputDataActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_data);
 
-        // Getting input num from last session if possible
+        // Getting bib num from last session if possible
         if (savedInstanceState != null) {
-            inputNum = savedInstanceState.getParcelable(INPUT_NUM_BUNDLE_KEY);
+            bibNumber = savedInstanceState.getParcelable(BIB_NUM_BUNDLE_KEY);
         }
 
         // Getting account from previous activity
-        Account mAccount = (Account) getIntent().getParcelableExtra("UserAccount");
+        Account mAccount = getIntent().getParcelableExtra("UserAccount");
         reader = new SheetReader(mAccount);
 
         // Getting views from ID
-        mInputNumberEditText = findViewById(R.id.input_data_choose_number);
+
+
+        mInputBibNumberEditText = findViewById(R.id.input_data_edit_bib_number);
         mOutputNameTextView = findViewById(R.id.input_data_name_output);
-        mOutputCompetitorTypeTextView = findViewById(R.id.input_data_competitor_type_output);
+        mOutputCompetitorTypeSpinner = findViewById(R.id.input_data_competitor_type_spinner);
+        mOutputOwnTimeCheckBox = findViewById(R.id.input_data_own_time_check_box);
+        mOutputOwnTimeEditText = findViewById(R.id.input_data_own_time_edit_time);
 
 
 
 
-        mInputNumberEditText.addTextChangedListener(new TextWatcher() {
+        mInputBibNumberEditText.addTextChangedListener(new TextWatcher() {
 
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -74,8 +85,38 @@ public class InputDataActivity extends Activity {
         List<Object> singleRowList = rowData.getValues().get(0);
         String name = (String) singleRowList.get(0);
         String competitorType = (String) singleRowList.get(1);
+        String ownTimeBoolean = (String) singleRowList.get(2);
+        String ownTimeRecordedTime = (String) singleRowList.get(3);
         mOutputNameTextView.append(name);
-        mOutputCompetitorTypeTextView.append(competitorType);
+        mOutputCompetitorTypeSpinner.setSelection(convertCompetitorTypeToInt(competitorType));
+        mOutputOwnTimeCheckBox.setChecked(convertOwnTimeToBoolean(ownTimeBoolean));
+        if (mOutputOwnTimeCheckBox.isChecked()) {
+            mOutputOwnTimeEditText.append(ownTimeRecordedTime);
+        }
+    }
+
+    private int convertCompetitorTypeToInt(String competitorType) {
+        switch (competitorType) {
+            case "ER":
+                return 0;
+            case "R":
+                return 1;
+            case "W":
+                return 2;
+            default:
+                throw new RuntimeException("Unexpected competitor type: " + competitorType);
+        }
+    }
+
+    private boolean convertOwnTimeToBoolean(String ownTimeBoolean) {
+        switch (ownTimeBoolean) {
+            case "":
+                return false;
+            case "ot":
+                return true;
+            default:
+                throw new RuntimeException("Unexpected own time boolean value: " + ownTimeBoolean);
+        }
     }
 
 }
